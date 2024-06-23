ea446e5c30ebd3f271e14aa5286dbac7349fc690
Sidak Singh <131273121+Sidak08@users.noreply.github.com>
2024-06-23 15:47:55 -0400

    Relearned trig and got 90% of the petal working. I have all the coordinates just need to add the curves.

--- main.js
+++ main.js
@@ -2,18 +2,65 @@ const width = 125
 const height = 125
 
 setDocDimensions(width, height)
-const innerCircle = new bt.Turtle()
-innerCircle.jump([42.5, 42.5])
-innerCircle.forward(20)
-innerCircle.left(90)
-innerCircle.forward(20)
-innerCircle.left(90)
-innerCircle.forward(20)
-innerCircle.left(90)
-innerCircle.forward(20)
+function getCirclePoints(h, k, r, n) {
+  const points = [];
+  const angleStep = (2 * Math.PI) / n; // Angle between points
+
+  for (let i = 0; i < n; i++) {
+    const angle = i * angleStep;
+    const x = h + r * Math.cos(angle);
+    const y = k + r * Math.sin(angle);
+    points.push({ x, y });
+  }
+
+  return points;
+}
+
+const genRanCir = (x, y, r, ran) => {
+  const circle = new bt.Turtle()
+  circle.jump([x, y])
+  circle.forward(r)
+  circle.left(90)
+  circle.forward(r)
+  circle.left(90)
+  circle.forward(r)
+  circle.left(90)
+  circle.forward(r)
+  const tmp = bt.catmullRom(circle.lines()[0])
+  const finCircle = bt.catmullRom(bt.resample([tmp], ran)[0])
+  return finCircle
+}
+
+const genPetal = (x, y, r, num) => {
+  r = r / 1.5
+  x = x + r * 0.75
+  y = y + r * 0.75
+  const circlePoints = getCirclePoints(x, y, r, num);
+  const outerCircPoints = getCirclePoints(x, y, r + 20, num);
+  for (let i = 0; i < circlePoints.length; i++) {
+    let xCor = circlePoints[i].x
+    let yCor = circlePoints[i].y
+    let xOutCor = outerCircPoints[i].x
+    let yOutCor = outerCircPoints[i].y
+    
+    const petal = new bt.Turtle()
+    petal.jump([xCor, yCor])
+    petal.goTo([x,y)
+    drawLines(petal.lines())
+  }
+}
 
 
-const curve = bt.catmullRom(innerCircle.lines()[0])
-console.log(bt.resample([curve], 8))
-const secondCurve = bt.catmullRom(bt.resample([curve], 10)[0])
-drawLines([secondCurve])
+const x = 40
+const y = 40
+const r = 10
+
+const a = genRanCir(x, y, r, Math.floor(Math.random() * 10))
+genPetal(x, y, r, 5)
+
+// bt.translate([a], [20, 20]) use full for later
+drawLines([a])
+
+
+
+
