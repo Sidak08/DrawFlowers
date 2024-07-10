94b0ddf6b75d0b08b16b1c169bc65c2d2c05a559
Sidak Singh <131273121+Sidak08@users.noreply.github.com>
2024-07-10 17:22:22 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,6 @@
 @snapshot: image2.png
 */
 
-
 const width = 125
 const height = 125
 setDocDimensions(width, height)
@@ -12,7 +11,7 @@ setDocDimensions(width, height)
 const r = Math.floor((Math.random() * 10) + 5)
 const x = (width - r) / 2 
 const y = 65
-const petalNum = Math.floor((Math.random() * 12) + 7)
+const petalNum = Math.floor((Math.random() * 8) + 7)
 const petalHeight = Math.floor((Math.random() * 20) + 20)
 const innerPetalNum = Math.floor((Math.random() * 7) + 5)
 const innerPetalHeight = Math.floor((Math.random() * 10) + 5)
@@ -55,7 +54,7 @@ const genPetal = (x, y, r, num, h) => {
   y = y + r * 0.75
   const circlePoints = getCirclePoints(x, y, r, num);
   const outerCircPoints = getCirclePoints(x, y, r + h, num);
-  
+
   const midCircPoints1 = getCirclePoints(x, y, r + ((h / 10)* 2.3), num, 5.9);
   const midCircPoints2 = getCirclePoints(x, y, r + ((h / 7)* 2.3), num, -5.9);
 
@@ -63,14 +62,14 @@ const genPetal = (x, y, r, num, h) => {
   const midHighCircPoints2 = getCirclePoints(x, y, r + ((h / 10)* 7), num, -5.9);
 
   const petalArray = []
-  
+
   for (let i = 0; i < circlePoints.length; i++) {
     let xCor = circlePoints[i].x
     let yCor = circlePoints[i].y
-    
+
     let xOutCor = outerCircPoints[i].x
     let yOutCor = outerCircPoints[i].y
-    
+
     let xMidCor1 = midCircPoints1[i].x
     let yMidCor1 = midCircPoints1[i].y
     let xMidCor2 = midCircPoints2[i].x
@@ -81,7 +80,7 @@ const genPetal = (x, y, r, num, h) => {
     let xMidHighCor2 = midHighCircPoints2[i].x
     let yMidHighCor2 = midHighCircPoints2[i].y
 
-    
+
     const petal = new bt.Turtle()
     petal.jump([xCor, yCor])
     petal.goTo([xMidCor1,yMidCor1])
@@ -97,24 +96,25 @@ const genPetal = (x, y, r, num, h) => {
   return petalArray
 }
 
-const genStem = (x, y, r, petalHeight) => {
+const genStem = (x, y, r, petalHeight, outterPetal) => {
   const offset = 8
-  const stem = new bt.Turtle()
+  let stem = new bt.Turtle()
   stem.jump([x + offset, y - petalHeight + 5])
   stem.goTo([x + offset, 10])
   stem.goTo([x + offset - 5, 10])
   stem.goTo([x + offset - 5, y - petalHeight + 5])
   stem.goTo([x + offset, y - petalHeight + 5])
-  drawLines([bt.catmullRom(stem.lines()[0])])
+  const newStem = bt.cover([bt.catmullRom(stem.lines()[0])], outterPetal)
+  drawLines(newStem)
 }
 
-const genFlower = (r, x, y, petalNum, PetalHeight, innerPetalNum, innerPetalHeight, circleMorph, innerCircleMorph) => {
+const genFlower = (r, x, y, petalNum, petalHeight, innerPetalNum, innerPetalHeight, circleMorph, innerCircleMorph) => {
   const outerCircle = genRanCir(x, y, r, circleMorph)
   const test = genRanCir(x + r / 2 , y + r / 2, r, circleMorph)
-  const innerCircle = genRanCir(x + 1.5, y + 1.5, r - 3, innerCircleMorph)
+  const innerCircle = genRanCir(x + 1.2, y + 1.2, r - 3, innerCircleMorph)
   const outterPetal = genPetal(x, y, r, petalNum, petalHeight)
   const innerPetal = genPetal(x, y, r, innerPetalNum, innerPetalHeight)
-  genStem(x, y, r, petalHeight)
+  genStem(x, y, r, petalHeight, outterPetal)
   drawLines([outerCircle])
 
   const newPetalDesign = bt.cover(outterPetal, innerPetal)
@@ -123,16 +123,3 @@ const genFlower = (r, x, y, petalNum, PetalHeight, innerPetalNum, innerPetalHeig
 }
 
 genFlower(r, x, y, petalNum, petalHeight, innerPetalNum, innerPetalHeight, circleMorph, innerCircleMorph)
-
-
-
-
-
-
-
-
-
-
-
-
-
